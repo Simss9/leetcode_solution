07b6d5713d9ae99da87d286ac297ae05f2980d90
Simran Khanna <khanna.simran.900@gmail.com>
2025-10-19 00:12:07 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (40.32%) - LeetSync

---
+++ DynamicProgramming_Leetcode/70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int climbStairs(int n) {
+        if( n == 0){
+            System.out.print(1);
+            return 1;
+        }
+        if( n == 1 ){
+            System.out.print(1);
+            return 1;
+        }
+          int prev1 = 1;
+          int prev2 = 1;
+          int curr = 0;
+          for(int i = 2 ;i<=n ; i++){
+            curr = prev1+prev2;
+            prev2 = prev1;
+            prev1 = curr;
+          }
+         return curr;
+          
+        }
+    }
+
