b359193b6aeeb9c6468b5eb42cc7f451f5adeb6b
Simran Khanna <khanna.simran.900@gmail.com>
2026-01-15 21:39:31 +0530

    Time: 8 ms (15.33%) | Memory: 49 MB (39.63%) - LeetSync

--- DynamicProgramming_Leetcode/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ DynamicProgramming_Leetcode/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,20 +1,53 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int size = nums1.length + nums2.length;
-        int[] merged = new int[size];
-        int k = 0;
-        for(int i = 0;i < nums1.length ; i++){
-            merged[k++] = nums1[i];
-        }
-        for(int i =0;i< nums2.length ; i++){
-            merged[k++] = nums2[i];
-        }
-        Arrays.sort(merged);
-        if(size % 2 !=0){
-            return merged[size / 2];
-        }else{
-            return(merged[size / 2 -1 ] + merged[size / 2] ) /2.0;
-        }
+      
+      
 
+        // Print statement (equivalent to cout)
+        System.out.print("SIMRAN(23BCS10153)");
+
+        // Always apply binary search on smaller array
+        if (nums1.length > nums2.length)
+            return findMedianSortedArrays(nums2, nums1);
+
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+
+        int low = 0, high = n1;
+
+        while (low <= high) {
+            int cut1 = (low + high) / 2;
+            int cut2 = (n1 + n2 + 1) / 2 - cut1;
+
+            int left1  = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1 - 1];
+            int left2  = (cut2 == 0) ? Integer.MIN_VALUE : nums2[cut2 - 1];
+
+            int right1 = (cut1 == n1) ? Integer.MAX_VALUE : nums1[cut1];
+            int right2 = (cut2 == n2) ? Integer.MAX_VALUE : nums2[cut2];
+
+            if (left1 <= right2 && left2 <= right1) {
+
+                // Even length
+                if ((n1 + n2) % 2 == 0) {
+                    return (Math.max(left1, left2) + Math.min(right1, right2)) / 2.0;
+                }
+                // Odd length
+                else {
+                    return Math.max(left1, left2);
+                }
+            }
+            else if (left1 > right2) {
+                high = cut1 - 1;
+            }
+            else {
+                low = cut1 + 1;
+            }
+        }
+        return 0.0;
+    
+
+
+       
+        
     }
 }
