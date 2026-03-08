46db1acb852774dc929f2dcdfa3c455ded445397
Simran Khanna <khanna.simran.900@gmail.com>
2026-03-08 15:38:44 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (87.90%) - LeetSync

---
+++ DynamicProgramming_Leetcode/2107-find-unique-binary-string/find-unique-binary-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String findDifferentBinaryString(String[] nums) {
+        int n = nums.length;
+        char[] result = new char[n];
+
+        // Flip the diagonal bits
+        for (int i = 0; i < n; i++) {
+            result[i] = (nums[i].charAt(i) == '0') ? '1' : '0';
+        }
+
+        return new String(result);
+    }
+}
